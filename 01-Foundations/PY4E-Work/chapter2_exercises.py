82e73c1d90ff36ece87dd0e515944990804be606
Dev-UK-Code <PANVHK55@GMAIL.COM>
2026-04-25 03:34:01 -0700

    Create chapter2_exercises.py
    feat: add greeting, pay calculator, temperature conversion

---
+++ 01-Foundations/PY4E-Work/chapter2_exercises.py
@@ -0,0 +1,23 @@
+1) Greeting Script
+
+name = input("Enter Your Name: ")
+print("Hello", name)
+
+
+2) The Basic Pay Calculator
+   calculates weekly pay based on user input
+
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+pay = float(hours) * float(rate)
+print("pay:", pay)
+
+converting input to float to multiply.
+
+3) Temperature Converter 
+   converts celciius into fahrenheit
+
+celsius = input("Enter celsius temperature: ")
+fahrenheit = (float(celcius) * 9/5 + 32
+print("fahrenheit temperature: ",fahrenheit)
+
